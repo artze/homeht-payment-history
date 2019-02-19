242b92298a87afbce715b5d725e36c57a2190872
artze <arthurwtm@gmail.com>
2019-02-19 14:31:20 +0000

    Implement routes and setup controllers for paymentsApi

---
+++ controllers/paymentsApi.js
@@ -0,0 +1,22 @@
+function getPayments(req, res) {
+    res.status(200).end()
+}
+
+function createPayment(req, res) {
+    res.status(201).end()
+}
+
+function updatePayment(req, res) {
+    res.status(200).end()
+}
+
+function deletePayment(req, res) {
+    res.status(200).end()
+}
+
+module.exports = {
+    getPayments,
+    createPayment,
+    updatePayment,
+    deletePayment
+}
