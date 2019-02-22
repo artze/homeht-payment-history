9e82a56f525a214e2f8d0f00bf90879fe7f8af33
artze <arthurwtm@gmail.com>
2019-02-22 13:36:18 +0000

    Fix date input to use ISO string instead of date objects

--- test/fixtures/paymentData.js
+++ test/fixtures/paymentData.js
@@ -2,7 +2,7 @@ const single = {
     contractId: '5a80785d802b546104b98714',
     description: 'database test',
     value: 300,
-    time: new Date('2018-12-31')
+    time: new Date('2018-12-31').toISOString()
 }
 
 const multiple = [
@@ -10,37 +10,37 @@ const multiple = [
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 300,
-        time: new Date('2018-01-31')
+        time: new Date('2018-01-31').toISOString()
     },
     {
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 370,
-        time: new Date('2018-02-01')
+        time: new Date('2018-02-01').toISOString()
     },
     {
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 400,
-        time: new Date('2018-02-15')
+        time: new Date('2018-02-15').toISOString()
     },
     {
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 600,
-        time: new Date('2018-03-02')
+        time: new Date('2018-03-02').toISOString()
     },
     {
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 300,
-        time: new Date('2018-04-02')
+        time: new Date('2018-04-02').toISOString()
     },
     {
         contractId: '9a80785d802b546104b987c3',
         description: 'database test',
         value: 400,
-        time: new Date('2018-06-01')
+        time: new Date('2018-06-01').toISOString()
     }
 ]
 
